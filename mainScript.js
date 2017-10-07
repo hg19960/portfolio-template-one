cf19190593e14859cb86b1400055cdd4f7efa74d
thehstudio <thehstudio@users.noreply.github.com>
2017-10-07 16:32:48 +0530

    portfolio-template-one

---
+++ mainScript.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	// body...
+	$('#menu-icon').click(fucntion(){
+		$(this).toggleClass("opa");
+		//getElementById('menu-icon').toggleClass('open');
+		//document.getElementById("menu-icon").classList.toggle("open");
+	});
+});
